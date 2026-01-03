fca739a7c7d87c1f9af827b225178f03db44bae1
oasisdg <hola@oasispozuelo.es>
2026-01-03 10:05:57 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; // <--- Importante
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import CookieBanner from './components/CookieBanner'; // <--- Nuevo Banner
+import CookieBanner from './components/CookieBanner';
 
 // Páginas
 import Home from './pages/Home';
@@ -10,8 +11,8 @@ import Spaces from './pages/Spaces';
 import Advantages from './pages/Advantages';
 import Pricing from './pages/Pricing';
 import Contact from './pages/Contact';
-import PrivacyPage from './pages/PrivacyPage'; // <--- Nueva Página
-import CookiesPage from './pages/CookiesPage'; // <--- Nueva Página
+import PrivacyPage from './pages/PrivacyPage';
+import CookiesPage from './pages/CookiesPage';
 
 // Scroll to top on route change
 const ScrollToTop = () => {
@@ -24,31 +25,31 @@ const ScrollToTop = () => {
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <ScrollToTop />
-      <div className="flex flex-col min-h-screen bg-[#f6f8f7] text-[#111814]">
-        <Navbar />
-        
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/espacios" element={<Spaces />} />
-            <Route path="/ventajas" element={<Advantages />} />
-            <Route path="/precios" element={<Pricing />} />
-            <Route path="/contacto" element={<Contact />} />
-            
-            {/* Rutas Legales */}
-            <Route path="/privacidad" element={<PrivacyPage />} />
-            <Route path="/cookies" element={<CookiesPage />} />
-          </Routes>
-        </main>
+    <HelmetProvider> {/* <--- El cerebro del SEO */}
+      <Router>
+        <ScrollToTop />
+        <div className="flex flex-col min-h-screen bg-[#f6f8f7] text-[#111814]">
+          <Navbar />
+          
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/espacios" element={<Spaces />} />
+              <Route path="/ventajas" element={<Advantages />} />
+              <Route path="/precios" element={<Pricing />} />
+              <Route path="/contacto" element={<Contact />} />
+              
+              {/* Rutas Legales */}
+              <Route path="/privacidad" element={<PrivacyPage />} />
+              <Route path="/cookies" element={<CookiesPage />} />
+            </Routes>
+          </main>
 
-        <Footer />
-        
-        {/* Banner de Cookies (flotante) */}
-        <CookieBanner />
-      </div>
-    </Router>
+          <Footer />
+          <CookieBanner />
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 };
 
