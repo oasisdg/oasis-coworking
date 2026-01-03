d8f7d97fa666e35d696dc2ae052c93503f71481b
oasisdg <hola@oasispozuelo.es>
2026-01-03 09:06:43 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-
 import React from 'react';
 import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import CookieBanner from './components/CookieBanner'; // <--- Nuevo Banner
+
+// Páginas
 import Home from './pages/Home';
 import Spaces from './pages/Spaces';
 import Advantages from './pages/Advantages';
 import Pricing from './pages/Pricing';
 import Contact from './pages/Contact';
+import PrivacyPage from './pages/PrivacyPage'; // <--- Nueva Página
+import CookiesPage from './pages/CookiesPage'; // <--- Nueva Página
 
 // Scroll to top on route change
 const ScrollToTop = () => {
@@ -24,6 +28,7 @@ const App: React.FC = () => {
       <ScrollToTop />
       <div className="flex flex-col min-h-screen bg-[#f6f8f7] text-[#111814]">
         <Navbar />
+        
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -31,9 +36,17 @@ const App: React.FC = () => {
             <Route path="/ventajas" element={<Advantages />} />
             <Route path="/precios" element={<Pricing />} />
             <Route path="/contacto" element={<Contact />} />
+            
+            {/* Rutas Legales */}
+            <Route path="/privacidad" element={<PrivacyPage />} />
+            <Route path="/cookies" element={<CookiesPage />} />
           </Routes>
         </main>
+
         <Footer />
+        
+        {/* Banner de Cookies (flotante) */}
+        <CookieBanner />
       </div>
     </Router>
   );
