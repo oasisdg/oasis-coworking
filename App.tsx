8c4c9ffa4a0bf93f8af6e6fa3cac2835f9b07def
oasisdg <hola@oasispozuelo.es>
2026-01-14 12:52:44 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,19 +4,11 @@ import { HelmetProvider } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import CookieBanner from './components/CookieBanner';
-import AnnouncementBar from './components/AnnouncementBar'; // <--- NUEVO
+import AnnouncementBar from './components/AnnouncementBar';
+import WelcomeModal from './components/WelcomeModal'; // <--- IMPORTAR AQUÍ
 
-// Páginas
-import Home from './pages/Home';
-import Spaces from './pages/Spaces';
-import Advantages from './pages/Advantages';
-import Pricing from './pages/Pricing';
-import Contact from './pages/Contact';
-import WaitingList from './pages/WaitingList'; // <--- NUEVO
-import PrivacyPage from './pages/PrivacyPage';
-import CookiesPage from './pages/CookiesPage';
+// ... (Resto de imports de páginas igual que antes) ...
 
-// Scroll to top on route change
 const ScrollToTop = () => {
   const { pathname } = useLocation();
   React.useEffect(() => {
@@ -31,19 +23,18 @@ const App: React.FC = () => {
       <Router>
         <ScrollToTop />
         <div className="flex flex-col min-h-screen bg-[#f6f8f7] text-[#111814]">
-          <AnnouncementBar /> {/* <--- BARRA AVISO APERTURA */}
+          <AnnouncementBar />
           <Navbar />
           
           <main className="flex-grow">
             <Routes>
+               {/* ... (Tus rutas siguen igual) ... */}
               <Route path="/" element={<Home />} />
               <Route path="/espacios" element={<Spaces />} />
               <Route path="/ventajas" element={<Advantages />} />
               <Route path="/precios" element={<Pricing />} />
               <Route path="/contacto" element={<Contact />} />
-              <Route path="/lista-espera" element={<WaitingList />} /> {/* <--- NUEVA RUTA */}
-              
-              {/* Rutas Legales */}
+              <Route path="/lista-espera" element={<WaitingList />} />
               <Route path="/privacidad" element={<PrivacyPage />} />
               <Route path="/cookies" element={<CookiesPage />} />
             </Routes>
@@ -51,6 +42,7 @@ const App: React.FC = () => {
 
           <Footer />
           <CookieBanner />
+          <WelcomeModal /> {/* <--- AÑADIR EL POPUP AQUÍ AL FINAL */}
         </div>
       </Router>
     </HelmetProvider>
